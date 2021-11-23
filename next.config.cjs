5860ac6646cd41a7b812dbdcd816b5eafe355955
David Lepaux <dlepaux@pubfac.com>
2021-11-23 23:48:34 +0100

    chore: fix next config

--- next.config.cjs
+++ next.config.cjs
@@ -1,3 +1,4 @@
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/realtime-bpm-analyzer/' : '/',
   basePath: process.env.NODE_ENV === 'production' ? '/realtime-bpm-analyzer/' : '/',
 };

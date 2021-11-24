622dd13ffa75f3b1896c94e3a63a5707a4bbc01e
David Lepaux <dlepaux@pubfac.com>
2021-11-24 10:33:44 +0100

    chore: prefix navbar links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ export default class extends React.Component {
       <>
         <Navbar bg="dark" style={{backgroundColor: '#2d333b !important'}} variant="dark" expand="sm">
           <Container fluid>
-            <Navbar.Brand href="/">
+            <Navbar.Brand href={`${process.env.PREFIX_URL}/`}>
               <img
                 alt=""
                 src={`${process.env.PREFIX_URL}/favicon.png`}
@@ -21,13 +21,13 @@ export default class extends React.Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="/api/index.html">API Documentation</Nav.Link>
+                <Nav.Link href={`${process.env.PREFIX_URL}/api/index.html`}>API Documentation</Nav.Link>
                 <NavDropdown title="Examples" menuVariant="dark" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="/audio-node">Audio Node</NavDropdown.Item>
-                  <NavDropdown.Item href="/user-media">User Media</NavDropdown.Item>
-                  <NavDropdown.Item href="/stream">Stream</NavDropdown.Item>
+                  <NavDropdown.Item href={`${process.env.PREFIX_URL}/audio-node`}>Audio Node</NavDropdown.Item>
+                  <NavDropdown.Item href={`${process.env.PREFIX_URL}/user-media`}>User Media</NavDropdown.Item>
+                  <NavDropdown.Item href={`${process.env.PREFIX_URL}/stream`}>Stream</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="/how-it-works">How it works</Nav.Link>
+                <Nav.Link href={`${process.env.PREFIX_URL}/how-it-works`}>How it works</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>

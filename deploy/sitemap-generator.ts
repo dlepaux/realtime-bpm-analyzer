18173a9aea3bb7ff2c2a0e372e115e573da7ee15
David Lepaux <d.lepaux@gmail.com>
2023-01-18 20:23:43 +0100

    chore: fix sitemap

--- deploy/sitemap-generator.ts
+++ deploy/sitemap-generator.ts
@@ -15,13 +15,10 @@ async function main() {
     }
   }
 
-  const xml = `
-<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages.map(page => `<url><loc>${baseUrl}${page}</loc></url>`).join('')}
-  </urlset>
-</xml>
-  `;
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${pages.map(page => `<url><loc>${baseUrl}${page}</loc></url>`).join('')}
+</urlset>`;
 
   await promises.writeFile('docs/sitemap.xml', xml, 'utf8');
 

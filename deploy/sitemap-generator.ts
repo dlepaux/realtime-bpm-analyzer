3e20de9f64a7f2a119883cae63fee34bdd724432
David Lepaux <d.lepaux@gmail.com>
2023-02-11 03:00:26 +0100

    chore: better sitemap gen

--- deploy/sitemap-generator.ts
+++ deploy/sitemap-generator.ts
@@ -29,8 +29,7 @@ async function main(): Promise<boolean> {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages.map((page: string) => `<url><loc>${baseUrl}${page}</loc><lastmod>${new Date().toISOString().split('T')[0]}</lastmod></url>`).join('')}
-  </urlset>
-</xml>`;
+  </urlset>`;
 
   await promises.writeFile('docs/sitemap.xml', xml, 'utf8');
 

29759bf7fdc3b8b379b1675c44f01c79f68be391
David Lepaux <d.lepaux@gmail.com>
2024-01-28 13:42:33 +0100

    chore: add coverage and clean up dependencies

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -14,6 +14,17 @@ export default {
       },
     }),
   ],
+  coverage: true,
+  coverageConfig: {
+    report: true,
+    reportDir: 'coverage',
+    threshold: {
+      statements: 70,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+    },
+  },
   plugins: [esbuildPlugin({
     ts: true,
   })],

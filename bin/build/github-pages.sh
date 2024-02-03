3a126568d2be61c6924d93cef1c454857586aea0
David Lepaux <d.lepaux@gmail.com>
2024-02-03 22:45:46 +0100

    chore: improve and fix generation of the documentation

--- bin/build/github-pages.sh
+++ bin/build/github-pages.sh
@@ -7,4 +7,5 @@ ts-node --esm --skipProject ./github-pages/favicons-generator.ts
 ts-node --esm --skipProject ./github-pages/sitemap-generator.ts
 ts-node --esm --skipProject ./github-pages/meta-injector.ts
 
+rm ./docs/favicons/index.html
 cp -r ./github-pages/public/* ./docs/
---
+++ github-pages/metadata.ts
@@ -0,0 +1,168 @@
+
+export const data: Array<{path: string; title: string; description: string}> = [
+  {
+    path: 'index.html',
+    title: 'Realtime BPM Analyzer Documentation',
+    description: 'Realtime BPM Analyzer provides efficient detection of BPM, in realtime and from files',
+  },
+  {
+    path: 'modules.html',
+    title: 'Realtime BPM Analyzer Modules',
+    description: 'Components of Realtime BPM Analyzer',
+  },
+  {
+    path: 'classes/RealTimeBpmAnalyzer.html',
+    title: 'Class RealTimeBpmAnalyzer',
+    description: 'This Class is used by the Realtime BPM Processor',
+  },
+  {
+    path: 'functions/analyzeFullBuffer.html',
+    title: 'Function analyzeFullBuffer',
+    description: 'Get the BPM of your files in a snap!',
+  },
+  {
+    path: 'functions/createRealTimeBpmProcessor.html',
+    title: 'AudioWorklet RealTimeBpmProcessor',
+    description: 'Create an audio worklet and compute BPM in realtime from a stream or a microphone',
+  },
+  {
+    path: 'functions/getBiquadFilters.html',
+    title: 'Function getBiquadFilters',
+    description: 'this handy function will permits you to create low and high filters easily.',
+  },
+  {
+    path: 'types/AggregateData.html',
+    title: 'Type AggregateData',
+    description: 'RealTimeBpmAnalyzer AggregateData Type',
+  },
+  {
+    path: 'types/AnalyzeChunkEvent.html',
+    title: 'Type AnalyzeChunkEvent',
+    description: 'RealTimeBpmAnalyzer AnalyzeChunkEvent Type',
+  },
+  {
+    path: 'types/AnalyzeChunkEventData.html',
+    title: 'Type AnalyzeChunkEventData',
+    description: 'RealTimeBpmAnalyzer AnalyzeChunkEventData Type',
+  },
+  {
+    path: 'types/AnalyzerResetedEvent.html',
+    title: 'Type AnalyzerResetedEvent',
+    description: 'RealTimeBpmAnalyzer AnalyzerResetedEvent Type',
+  },
+  {
+    path: 'types/AnalyzerResetedEventData.html',
+    title: 'Type AnalyzerResetedEventData',
+    description: 'RealTimeBpmAnalyzer AnalyzerResetedEventData Type',
+  },
+  {
+    path: 'types/RealtimeBpmAnalyzerEvents.html',
+    title: 'Type RealtimeBpmAnalyzerEvents',
+    description: 'RealTimeBpmAnalyzer RealtimeBpmAnalyzerEvents Type',
+  },
+  {
+    path: 'types/BiquadFilterOptions.html',
+    title: 'Type BiquadFilterOptions',
+    description: 'RealTimeBpmAnalyzer BiquadFilterOptions Type',
+  },
+  {
+    path: 'types/BpmCandidates.html',
+    title: 'Type BpmCandidates',
+    description: 'RealTimeBpmAnalyzer BpmCandidates Type',
+  },
+  {
+    path: 'types/BpmEvent.html',
+    title: 'Type BpmEvent',
+    description: 'RealTimeBpmAnalyzer BpmEvent Type',
+  },
+  {
+    path: 'types/BpmEventData.html',
+    title: 'Type BpmEventData',
+    description: 'RealTimeBpmAnalyzer BpmEventData Type',
+  },
+  {
+    path: 'types/Group.html',
+    title: 'Type Group',
+    description: 'RealTimeBpmAnalyzer Group Type',
+  },
+  {
+    path: 'types/Interval.html',
+    title: 'Type Interval',
+    description: 'RealTimeBpmAnalyzer Interval Type',
+  },
+  {
+    path: 'types/NextIndexPeaks.html',
+    title: 'Type NextIndexPeaks',
+    description: 'RealTimeBpmAnalyzer NextIndexPeaks Type',
+  },
+  {
+    path: 'types/NormalizedFilters.html',
+    title: 'Type NormalizedFilters',
+    description: 'RealTimeBpmAnalyzer NormalizedFilters Type',
+  },
+  {
+    path: 'types/OnThresholdFunction.html',
+    title: 'Type OnThresholdFunction',
+    description: 'RealTimeBpmAnalyzer OnThresholdFunction Type',
+  },
+  {
+    path: 'types/Peaks.html',
+    title: 'Type Peaks',
+    description: 'RealTimeBpmAnalyzer Peaks Type',
+  },
+  {
+    path: 'types/PeaksAndThreshold.html',
+    title: 'Type PeaksAndThreshold',
+    description: 'RealTimeBpmAnalyzer PeaksAndThreshold Type',
+  },
+  {
+    path: 'types/PostMessageEventData.html',
+    title: 'Type PostMessageEventData',
+    description: 'RealTimeBpmAnalyzer PostMessageEventData Type',
+  },
+  {
+    path: 'types/RealTimeBpmAnalyzerOptions.html',
+    title: 'Type RealTimeBpmAnalyzerOptions',
+    description: 'RealTimeBpmAnalyzer RealTimeBpmAnalyzerOptions Type',
+  },
+  {
+    path: 'types/RealTimeBpmAnalyzerParameters.html',
+    title: 'Type RealTimeBpmAnalyzerParameters',
+    description: 'RealTimeBpmAnalyzer RealTimeBpmAnalyzerParameters Type',
+  },
+  {
+    path: 'types/ResetEventData.html',
+    title: 'Type ResetEventData',
+    description: 'RealTimeBpmAnalyzer ResetEventData Type',
+  },
+  {
+    path: 'types/StopEventData.html',
+    title: 'Type StopEventData',
+    description: 'RealTimeBpmAnalyzer StopEventData Type',
+  },
+  {
+    path: 'types/Tempo.html',
+    title: 'Type Tempo',
+    description: 'RealTimeBpmAnalyzer Tempo Type',
+  },
+  {
+    path: 'types/Threshold.html',
+    title: 'Type Threshold',
+    description: 'RealTimeBpmAnalyzer Threshold Type',
+  },
+  {
+    path: 'types/ValidPeaks.html',
+    title: 'Type ValidPeaks',
+    description: 'RealTimeBpmAnalyzer ValidPeaks Type',
+  },
+  {
+    path: 'types/ValidPeakEvent.html',
+    title: 'Type ValidPeakEvent',
+    description: 'RealTimeBpmAnalyzer ValidPeakEvent Type',
+  },
+  {
+    path: 'types/ValidPeakEventData.html',
+    title: 'Type ValidPeakEventData',
+    description: 'RealTimeBpmAnalyzer ValidPeakEventData Type',
+  },
+];

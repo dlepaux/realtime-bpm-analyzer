552f197b6d63e88ce8cc93670ae5d882bf959574
David Lepaux <d.lepaux@gmail.com>
2025-11-23 00:37:05 +0100

    chore: add favicon to doc

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,6 +23,12 @@ export default defineConfig({
     ['meta', { name: 'og:type', content: 'website' }],
     ['meta', { name: 'og:locale', content: 'en' }],
     ['meta', { name: 'og:site_name', content: 'Realtime BPM Analyzer' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'Realtime BPM Analyzer' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest'}],
   ],
 
 

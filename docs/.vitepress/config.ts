5f1f387510759e708cff4e93a9746cc873c40370
David Lepaux <d.lepaux@gmail.com>
2025-11-23 00:42:03 +0100

    chore: add favicon to doc

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,17 +18,16 @@ export default defineConfig({
   ignoreDeadLinks: [],
   
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { name: 'theme-color', content: '#646cff' }],
     ['meta', { name: 'og:type', content: 'website' }],
     ['meta', { name: 'og:locale', content: 'en' }],
     ['meta', { name: 'og:site_name', content: 'Realtime BPM Analyzer' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' }],
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
-    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
     ['meta', { name: 'apple-mobile-web-app-title', content: 'Realtime BPM Analyzer' }],
-    ['link', { rel: 'manifest', href: '/site.webmanifest'}],
+    ['link', { rel: 'manifest', href: '/favicon/site.webmanifest'}],
   ],
 
 
